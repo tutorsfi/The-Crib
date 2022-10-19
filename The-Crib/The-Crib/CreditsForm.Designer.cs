38669ffbedf6be27f23f87f63adb447e02229a5f
Karoliina Salo <salo.karoliina@gmail.com>
2022-10-19 11:44:16 +0300

    Fixed language-selection in some forms / 19.10.22 / K.

--- The-Crib/The-Crib/CreditsForm.Designer.cs
+++ The-Crib/The-Crib/CreditsForm.Designer.cs
@@ -35,6 +35,8 @@
             this.GraphicsNamesLB = new System.Windows.Forms.Label();
             this.TutorsLogoPB = new System.Windows.Forms.PictureBox();
             this.MainMenuBtn = new System.Windows.Forms.Button();
+            this.TranslationsLB = new System.Windows.Forms.Label();
+            this.TranslatorsLB = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.TutorsLogoPB)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,7 +57,7 @@
             this.ProgrammingLB.BackColor = System.Drawing.Color.Transparent;
             this.ProgrammingLB.Font = new System.Drawing.Font("Cooper Black", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ProgrammingLB.ForeColor = System.Drawing.Color.White;
-            this.ProgrammingLB.Location = new System.Drawing.Point(403, 188);
+            this.ProgrammingLB.Location = new System.Drawing.Point(270, 191);
             this.ProgrammingLB.Name = "ProgrammingLB";
             this.ProgrammingLB.Size = new System.Drawing.Size(349, 82);
             this.ProgrammingLB.TabIndex = 1;
@@ -67,7 +69,7 @@
             this.GraphicsLB.BackColor = System.Drawing.Color.Transparent;
             this.GraphicsLB.Font = new System.Drawing.Font("Cooper Black", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GraphicsLB.ForeColor = System.Drawing.Color.White;
-            this.GraphicsLB.Location = new System.Drawing.Point(1053, 188);
+            this.GraphicsLB.Location = new System.Drawing.Point(734, 191);
             this.GraphicsLB.Name = "GraphicsLB";
             this.GraphicsLB.Size = new System.Drawing.Size(178, 40);
             this.GraphicsLB.TabIndex = 2;
@@ -79,7 +81,7 @@
             this.ProgrammerNamesLB.BackColor = System.Drawing.Color.Transparent;
             this.ProgrammerNamesLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ProgrammerNamesLB.ForeColor = System.Drawing.Color.White;
-            this.ProgrammerNamesLB.Location = new System.Drawing.Point(404, 281);
+            this.ProgrammerNamesLB.Location = new System.Drawing.Point(271, 284);
             this.ProgrammerNamesLB.Name = "ProgrammerNamesLB";
             this.ProgrammerNamesLB.Size = new System.Drawing.Size(253, 155);
             this.ProgrammerNamesLB.TabIndex = 5;
@@ -91,11 +93,11 @@
             this.GraphicsNamesLB.BackColor = System.Drawing.Color.Transparent;
             this.GraphicsNamesLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GraphicsNamesLB.ForeColor = System.Drawing.Color.White;
-            this.GraphicsNamesLB.Location = new System.Drawing.Point(1043, 281);
+            this.GraphicsNamesLB.Location = new System.Drawing.Point(724, 284);
             this.GraphicsNamesLB.Name = "GraphicsNamesLB";
-            this.GraphicsNamesLB.Size = new System.Drawing.Size(234, 124);
+            this.GraphicsNamesLB.Size = new System.Drawing.Size(234, 93);
             this.GraphicsNamesLB.TabIndex = 6;
-            this.GraphicsNamesLB.Text = "Adelina Strapchuk\r\nHugo Koukka\r\nName\r\nName\r\n";
+            this.GraphicsNamesLB.Text = "Adelina Strapchuk\r\nHugo Koukka\r\n\r\n";
             // 
             // TutorsLogoPB
             // 
@@ -122,12 +124,38 @@
             this.MainMenuBtn.UseVisualStyleBackColor = false;
             this.MainMenuBtn.Click += new System.EventHandler(this.MainMenuBtn_Click);
             // 
+            // TranslationsLB
+            // 
+            this.TranslationsLB.AutoSize = true;
+            this.TranslationsLB.BackColor = System.Drawing.Color.Transparent;
+            this.TranslationsLB.Font = new System.Drawing.Font("Cooper Black", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TranslationsLB.ForeColor = System.Drawing.Color.White;
+            this.TranslationsLB.Location = new System.Drawing.Point(1032, 191);
+            this.TranslationsLB.Name = "TranslationsLB";
+            this.TranslationsLB.Size = new System.Drawing.Size(243, 40);
+            this.TranslationsLB.TabIndex = 10;
+            this.TranslationsLB.Text = "Translations";
+            // 
+            // TranslatorsLB
+            // 
+            this.TranslatorsLB.AutoSize = true;
+            this.TranslatorsLB.BackColor = System.Drawing.Color.Transparent;
+            this.TranslatorsLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TranslatorsLB.ForeColor = System.Drawing.Color.White;
+            this.TranslatorsLB.Location = new System.Drawing.Point(1041, 271);
+            this.TranslatorsLB.Name = "TranslatorsLB";
+            this.TranslatorsLB.Size = new System.Drawing.Size(241, 62);
+            this.TranslatorsLB.TabIndex = 11;
+            this.TranslatorsLB.Text = "Dima Shkoliarenko\r\nName\r\n";
+            // 
             // CreditsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::The_Crib.Properties.Resources.Frontyard;
             this.ClientSize = new System.Drawing.Size(1634, 891);
+            this.Controls.Add(this.TranslatorsLB);
+            this.Controls.Add(this.TranslationsLB);
             this.Controls.Add(this.MainMenuBtn);
             this.Controls.Add(this.TutorsLogoPB);
             this.Controls.Add(this.GraphicsNamesLB);
@@ -153,5 +181,7 @@
         private System.Windows.Forms.Label GraphicsNamesLB;
         private System.Windows.Forms.PictureBox TutorsLogoPB;
         private System.Windows.Forms.Button MainMenuBtn;
+        private System.Windows.Forms.Label TranslationsLB;
+        private System.Windows.Forms.Label TranslatorsLB;
     }
 }
